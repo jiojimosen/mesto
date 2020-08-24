ae2b56492dff233135a4a726dd38b9bd7820d817
Mikhail Resnov <mresnov@yandex.ru>
2020-08-24 19:01:28 +0300

    Another commit

--- script/card-add.js
+++ script/card-add.js
@@ -1,26 +1,17 @@
 let cardAdd = document.querySelector(".card-add");
 let editCardAdd = document.querySelector(".profile__add-button");
 let closeCardAdd = document.querySelector(".card-add__container-cross");
-//let name = document.querySelector(".profile__title");
-//let description = document.querySelector(".profile__subtitle");
 let cardName = document.querySelector("#card-name");
 let cardLink = document.querySelector("#card-link");
 let cardElement = document.querySelector("#card");
 
 function cardAddOpen () {
     cardAdd.classList.toggle('card-add_opened');
-    //if (popup.classList.contains('card-add_opened')) {
-       // formName.value = name.textContent
-       // formDescription.value = description.textContent
-   // }
 };
 
 function cardSubmitHandler (evt) {
     evt.preventDefault(); 
 
-    //name.textContent = formName.value;
-    //description.textContent = formDescription.value;
- 
     const cardTemplate = document.querySelector('#card-template').content;
     const cardContent =  cardTemplate.cloneNode(true);
     const cardUnit = document.querySelector('.elements');
@@ -28,6 +19,10 @@ function cardSubmitHandler (evt) {
     cardContent.querySelector('.elements__card-image').src = cardLink.value;
     cardContent.querySelector('.elements__card-image').alt = cardName.value;
     cardContent.querySelector('.elements__card-title').textContent = cardName.value;
+    
+    cardContent.querySelector('.elements__like').addEventListener('click', function(evt) {
+        evt.target.classList.toggle('elements__like_liked');
+    });
 
     if ((cardLink.value <= 0) || (cardName.value <= 0)) {
         alert('Форма должна быть заполнена');
@@ -39,10 +34,8 @@ function cardSubmitHandler (evt) {
     cardLink.value = "";
     cardName.value = "";
     cardAddOpen();
-   
 };
 
 editCardAdd.addEventListener('click', cardAddOpen);
 closeCardAdd.addEventListener('click', cardAddOpen);
 cardElement.addEventListener('submit', cardSubmitHandler);
-
--- script/card-init.js
+++ script/card-init.js
@@ -25,39 +25,18 @@ const initialCards = [
     }
 ];
 
-
-/*const cardsContainer = document.querySelector('.elements');
-
-const nameCard = initialCards.map(function (item) {
-    return item.name;
-});
-
-const linkCard = initialCards.map(function (item) {
-    return item.link;
-});
-
-
-function loadCard() {
-    const cardTemplate = document.querySelector('#card-template').content;
-    const cardUnit = document.querySelector('.elements');
-
-    const cardContent = cardTemplate.cloneNode(true);
-
-    cardUnit.querySelector('.elements__card-image').src = linkCard;
-    cardUnit.querySelector('.elements__card-title').textContent = getName;
-
-    cardUnit.append(cardContent);
-};
-
-initialCards.forEach(loadCard);*/
+const cardUnit = document.querySelector('.elements');
 
 const newCard = card => {
     const cardTemplate = document.querySelector('#card-template').content;
     const cardContent =  cardTemplate.cloneNode(true);
-    const cardUnit = document.querySelector('.elements');
     cardContent.querySelector('.elements__card-image').src = card.link;
     cardContent.querySelector('.elements__card-image').alt = card.name;
     cardContent.querySelector('.elements__card-title').textContent = card.name;
+
+    cardContent.querySelector('.elements__like').addEventListener('click', function(evt) {
+        evt.target.classList.toggle('elements__like_liked');
+    });
 
     cardUnit.append(cardContent);
 };
